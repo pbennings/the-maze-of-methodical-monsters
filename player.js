f6d86ed8735ac6c9c7179f68d49e97560d63e5a7
Paul Bennings <paul.bennings@codeimmersives.com>
2019-04-04 00:11:13 -0400

    finishing up functions

--- player.js
+++ player.js
@@ -1,9 +1,14 @@
 const player = {
+  isAlive: true,
+  hitPoints: 100,
+  level: 1,
   lastDamageTaken: 0,
+  flight: function(monster) {
+    this.lastDamageTaken =
+  }
+
   levelUp: function() {
   this.levelUp = true;
   this.hitPoints = 100;
   },
-  hitPoints: 100,
-  isAlive: true,
 }
